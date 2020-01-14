e388a181f42cd79f792b6e7792ed55fc813eb2ef
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2020-01-14 09:58:42 +0900

    Parse csv

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-'use strict';
+const csv = require('csv-parser');
+const fs = require('fs');
+const results = [];
+
+const main = () => {
+  fs.createReadStream('assets/toilet.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+      results.forEach(result => {
+        console.log(result['ID']);
+      })
+      console.log(`計: ${results.length}件`)
+    });
+}
+
+main();
